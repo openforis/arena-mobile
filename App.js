eddebd044b06724e14ea0a49d29a0d4fe6ca449b
Ramiro Blázquez <ramirobg94@gmail.com>
2022-01-11 11:32:51 +0100

    move app

---
+++ App.js
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import App from './App';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hello arena</Text>
+    </View>
+  );
+};
+
+export default App;
