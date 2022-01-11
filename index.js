62de94166fb8bb7996e7fdc215bcf5c5438c6de2
Ramiro Blázquez <me@ramiroblazquez.com>
2022-01-11 11:32:51 +0100

    move app

--- index.js
+++ index.js
@@ -2,11 +2,8 @@
  * @format
  */
 
-import {AppRegistry, View, Text } from 'react-native';
-
+import {AppRegistry} from 'react-native';
+import App from './App';
 import {name as appName} from './app.json';
 
-const App = () => {
-    return <View><Text>Hello arena</Text></View>
-}
 AppRegistry.registerComponent(appName, () => App);
