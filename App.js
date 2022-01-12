94e76af33bd50188a83d5de8e551d9db0fcd88ae
Ramiro Blázquez <me@ramiroblazquez.com>
2022-01-12 13:15:42 +0100

    start navigation

--- App.js
+++ App.js
@@ -1,12 +1,1 @@
-import {View, Text} from 'react-native';
-import App from './App';
-
-const App = () => {
-  return (
-    <View>
-      <Text>Hello arena</Text>
-    </View>
-  );
-};
-
-export default App;
+export {default} from 'navigation';
