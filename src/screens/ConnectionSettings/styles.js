f1be3cb46aab7b9cb9f4a5d49d5e8732cc23e670
Ramiro Blázquez <ramirobg94@gmail.com>
2022-01-14 18:37:10 +0100

    Add settings scren with back navigation

---
+++ src/screens/ConnectionSettings/styles.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  header: {
+    fontSize: 18,
+    fontWeight: 'bold'
+  },
+});
+
+export default styles;
