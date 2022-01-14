8ceed45038eef68aa3e730d0362cece32d5c08f0
Ramiro Blázquez <ramirobg94@gmail.com>
2022-01-14 13:21:15 +0100

    Create Header component with Touchable icons with iconicons

---
+++ src/arena-mobile-ui/components/TouchableIcons/TouchableIcon/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {TouchableOpacity} from 'react-native';
+
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+const TouchableIcon = ({onPress, iconName, size = 24, customStyle = {}, iconColor = null }) => {
+  return (
+    <TouchableOpacity onPress={onPress} style={[customStyle]}>
+      <Ionicons name={iconName} size={size} color={iconColor}/>
+    </TouchableOpacity>
+  );
+};
+
+export default TouchableIcon;
