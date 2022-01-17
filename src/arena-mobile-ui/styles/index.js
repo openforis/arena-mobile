0e623d9e0e128101b02bb32105d89f803226e068
Ramiro Blázquez <me@ramiroblazquez.com>
2022-01-17 12:43:17 +0100

    Base text styles, navigate to settings icon, home with layout

---
+++ src/arena-mobile-ui/styles/index.js
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import {StyleSheet} from 'react-native';
+
+const textStyle = StyleSheet.create({
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  header: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  text: {
+    fontSize: 14,
+  },
+  bold: {
+    fontSize: 14,
+    fontWeight: 'bold',
+  },
+});
+
+
+
+export default {
+  textStyle,
+};
