53cfb69886813b1086648da3416a4d5977299510
Ramiro Blázquez <me@ramiroblazquez.com>
2022-01-20 17:21:13 +0100

    add settings navigation constants

--- src/navigation/constants/index.js
+++ src/navigation/constants/index.js
@@ -1,12 +1,18 @@
 import * as Home from 'screens/Home';
+import * as ConnectionSettings from 'screens/ConnectionSettings';
 
 export const KEYS = {
   [Home.key]: [Home.key],
-};
+  [ConnectionSettings.key]: [ConnectionSettings.key]
+}
 export const SCREENS = {
   [Home.key]: Home.screenConfig,
+  [ConnectionSettings.key]: ConnectionSettings.screenConfig
 };
 
 export const ROUTES = {
   [Home.key]: Home.route,
+  [ConnectionSettings.key]: ConnectionSettings.route
 };
+
+
