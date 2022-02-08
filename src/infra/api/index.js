becfeb5a6e77c0e4b75c87076b8031161504f72b
Ramiro Blázquez <ramirobg94@gmail.com>
2022-02-08 05:41:21 -0500

    Fetch/api/axios (#40)

---
+++ src/infra/api/index.js
@@ -0,0 +1,72 @@
+import axios from 'axios'
+
+const SERVER_URL = 'https://2abd-213-177-210-99.ngrok.io'
+
+const CONTENT_TYPES = {
+  json: 'application/json',
+  jpg: 'image/jpeg',
+}
+
+export default ({serverUrl = SERVER_URL}) => {
+  const BASE_URL = serverUrl
+  console.log('API::serverUrl::', serverUrl)
+
+  const fetchGeneric = async ({
+    method,
+    body,
+    path,
+    uri = false,
+    contentType = '',
+  }) =>
+    axios(uri ? uri : `${BASE_URL}/${path}`, {
+      method,
+      ...(body ? {body: JSON.stringify(body)} : {}),
+      ...(body ? {data: JSON.stringify(body)} : {}), // body in axios is params
+      headers: {
+        'Content-Type': contentType || CONTENT_TYPES.json,
+      },
+    })
+
+  const get = async ({body, path, uri, contentType}) =>
+    fetchGeneric({
+      method: 'GET',
+      body,
+      path,
+      uri,
+      contentType,
+    })
+
+  const patch = async ({body, path, uri, contentType}) =>
+    fetchGeneric({
+      method: 'PATCH',
+      body,
+      path,
+      uri,
+      contentType,
+    })
+
+  const put = async ({body, path, uri, contentType}) =>
+    fetchGeneric({
+      method: 'PUT',
+      body,
+      path,
+      uri,
+      contentType,
+    })
+
+  const post = async ({body, path, uri, contentType}) =>
+    fetchGeneric({
+      method: 'POST',
+      body,
+      path,
+      uri,
+      contentType,
+    })
+
+  return {
+    get,
+    patch,
+    put,
+    post,
+  }
+}
