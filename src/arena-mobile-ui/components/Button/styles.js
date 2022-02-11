bbc623c48323ea984fbcc87f394682accf4b1b61
Ramiro Blázquez <me@ramiroblazquez.com>
2022-02-11 12:37:15 -0500

    add some components

---
+++ src/arena-mobile-ui/components/Button/styles.js
@@ -0,0 +1,54 @@
+import * as React from 'react'
+import {StyleSheet} from 'react-native'
+import * as colors from 'arena-mobile-ui/colors'
+
+const styles = StyleSheet.create({
+  base: {
+    /*borderColor: colors.greyBorder,
+    borderWidth: 1,*/
+
+    padding: 8,
+    paddingVertical: 12,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 8,
+  },
+  primary: {
+    backgroundColor: colors.blue,
+  },
+  secondary: {
+    backgroundColor: colors.white,
+  },
+  delete: {
+    backgroundColor: colors.red,
+  },
+  ghost: {
+    backgroundColor: 'transparent',
+  },
+  ghostBlack: {
+    backgroundColor: 'transparent',
+  },
+  text: {
+    base: {
+      fontSize: 14,
+      fontWeight: 'bold',
+    },
+    primary: {
+      color: colors.white,
+    },
+    secondary: {
+      color: colors.black,
+    },
+    delete: {
+      color: colors.white,
+    },
+    ghost: {
+      backgroundColor: colors.blue,
+    },
+    ghostBlack: {
+      backgroundColor: colors.black,
+    },
+  },
+})
+
+export default styles
