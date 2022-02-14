2efa7963222cf980a1863989dd8e00b90212e46c
Ramiro Blázquez <ramirobg94@gmail.com>
2022-02-14 17:05:11 -0500

    initialize settings screen

--- src/screens/ConnectionSettings/screen.js
+++ src/screens/ConnectionSettings/screen.js
@@ -1,21 +1,91 @@
-import React from 'react';
+import React, {useCallback, useEffect, useState} from 'react'
 
-import {Text} from 'react-native';
+import {
+  Text,
+  View,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+} from 'react-native'
 
-import Layout from 'arena-mobile-ui/components/Layout';
-import Header from 'arena-mobile-ui/components/Header';
-import styles from './styles';
+import {useDispatch, useSelector} from 'react-redux'
+
+import {selectors as appSelectors, actions as appActions} from 'state/app'
+
+import Layout from 'arena-mobile-ui/components/Layout'
+import Header from 'arena-mobile-ui/components/Header'
+import Input from 'arena-mobile-ui/components/Input'
+import Button from 'arena-mobile-ui/components/Button'
+
+import baseStyles from 'arena-mobile-ui/styles'
+
+import styles from './styles'
 
 const ConnectionSettings = () => {
+  const [formData, setFormData] = useState({})
+  const dispatch = useDispatch()
+
+  const onChangeText = useCallback(
+    key => value => {
+      setFormData(prevValue => ({...prevValue, [key]: value}))
+    },
+    [],
+  )
+
+  const handleSubmitForm = useCallback(() => {
+    dispatch(appActions.initConnection(formData))
+  }, [formData])
+
+  const {username, password} = useSelector(appSelectors.getAccessData)
+
+  const isLoading = useSelector(appSelectors.getIsLoading)
+
+  useEffect(() => {
+    setFormData(prevData => ({
+      ...prevData,
+      username: username || prevData.username,
+      password: password || prevData.password,
+    }))
+  }, [username, password])
+
   return (
     <Layout>
-      <Header hasBackComponent>
-        <Text>Connection settings</Text>
-      </Header>
+      <>
+        <Header hasBackComponent>
+          <Text style={[baseStyles.textStyle.title]}>Connection settings</Text>
+        </Header>
 
-      <Text style={[styles.header]}>Server config</Text>
+        <KeyboardAvoidingView
+          style={styles.container}
+          behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+          <ScrollView>
+            <View style={{flex: 1, padding: 16}}>
+              <Text style={[styles.header]}>Access info</Text>
+
+              <Input
+                title="Username (email)"
+                onChangeText={onChangeText('username')}
+                value={formData?.username || username}
+              />
+              <Input
+                title="Password"
+                onChangeText={onChangeText('password')}
+                value={formData?.password || password}
+                secureTextEntry={true}
+              />
+
+              <Button
+                onPress={handleSubmitForm}
+                label="connect"
+                disabled={isLoading}
+              />
+              <View style={{height: 100}}></View>
+            </View>
+          </ScrollView>
+        </KeyboardAvoidingView>
+      </>
     </Layout>
-  );
-};
+  )
+}
 
-export default ConnectionSettings;
+export default ConnectionSettings
