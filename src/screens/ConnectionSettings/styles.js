47f8f83057743ac4c75fc1da3fd72af61acd5335
Ramiro Blázquez <ramirobg94@gmail.com>
2022-03-01 13:15:38 +0100

    allow multiple servers

--- src/screens/ConnectionSettings/styles.js
+++ src/screens/ConnectionSettings/styles.js
@@ -1,31 +1,32 @@
-import * as React from 'react';
-import {StyleSheet} from 'react-native';
+import * as React from 'react'
+import {StyleSheet} from 'react-native'
+import * as colors from 'arena-mobile-ui/colors'
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
+    backgroundColor: colors.background,
   },
   title: {
     fontSize: 24,
-    fontWeight: 'bold'
+    fontWeight: 'bold',
   },
   header: {
     fontSize: 18,
-    fontWeight: 'bold'
+    fontWeight: 'bold',
   },
   text: {
-    fontSize: 14
+    fontSize: 14,
   },
   input: {
-    backgroundColor: 'white',
+    backgroundColor: colors.white,
     padding: 8,
     paddingVertical: 12,
     fontSize: 14,
     borderWidth: 1,
     borderColor: '#d1d1dd',
-    marginVertical: 4
-  }
-});
+    marginVertical: 4,
+  },
+})
 
-export default styles;
+export default styles
