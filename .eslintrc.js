02b5859fce8adaf8e81fecd4346bdb0c22c51833
Ramiro Blázquez <me@ramiroblazquez.com>
2022-03-04 18:12:36 +0100

    eslint order

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,29 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: ['@react-native-community', 'plugin:import/recommended'],
   parser: '@babel/eslint-parser',
-}
+  plugins: ['import'],
+  rules: {
+    'import/no-unresolved': [2, {commonjs: true, amd: true}],
+    'import/named': 2,
+    'import/namespace': 2,
+    'import/default': 2,
+    'import/export': 2,
+
+    'import/order': [
+      'error',
+
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+          'object',
+        ],
+      },
+    ],
+  },
+};
