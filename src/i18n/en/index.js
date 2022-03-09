a96c6365a0b8ed552c357d76a95112a51f7ad4a8
Ramiro Blázquez <me@ramiroblazquez.com>
2022-03-09 12:31:08 +0100

    QR reader

--- src/i18n/en/index.js
+++ src/i18n/en/index.js
@@ -1,9 +1,13 @@
 import Common from './Common';
+import ConnectionSettings from './ConnectionSettings';
 import Home from './Home';
+import QRScanner from './QRScanner';
 
 const lang = {
   Common,
+  ConnectionSettings,
   Home,
+  QRScanner,
 };
 
 export default lang;
