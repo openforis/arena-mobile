a733763146281811e692b5df581a77d2b24a8d80
Ramiro Blázquez <ramirobg94@gmail.com>
2022-03-11 17:11:40 +0100

    style fixes

--- src/arena-mobile-ui/styles/index.js
+++ src/arena-mobile-ui/styles/index.js
@@ -4,21 +4,17 @@ import * as colors from '../colors';
 
 const textStyle = StyleSheet.create({
   title: {
-    textTransform: 'capitalize',
     fontSize: 24,
     fontWeight: 'bold',
   },
   header: {
-    textTransform: 'capitalize',
     fontSize: 18,
     fontWeight: 'bold',
   },
   text: {
-    textTransform: 'capitalize',
     fontSize: 14,
   },
   bold: {
-    textTransform: 'capitalize',
     fontSize: 14,
     fontWeight: 'bold',
   },
