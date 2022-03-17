a0a4c25f5a0f248d940e117f8cddd3b31253aa2f
Ramiro Blázquez <ramirobg94@gmail.com>
2022-03-17 12:27:22 +0100

    fixes

--- src/arena-mobile-ui/components/Layout/index.js
+++ src/arena-mobile-ui/components/Layout/index.js
@@ -3,12 +3,12 @@ import {View, SafeAreaView, StatusBar} from 'react-native';
 
 import styles from './styles';
 
-const Layout = ({children}) => {
+const Layout = ({children, bottomStyle = 'white'}) => {
   return (
     <View style={[styles.container]}>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView />
-      <SafeAreaView style={[styles.container, styles.bottom]}>
+      <SafeAreaView style={[styles.container, styles.bottom[bottomStyle]]}>
         {children}
       </SafeAreaView>
     </View>
