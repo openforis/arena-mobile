cc522aca1694927a30150d197228b2db4027ac47
Ramiro Blázquez <ramirobg94@gmail.com>
2022-03-21 09:48:35 +0100

    add empty component

---
+++ src/arena-mobile-ui/components/Empty/styles.js
@@ -0,0 +1,16 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-between',
+    alignContent: 'center',
+    padding: 16,
+    paddingTop: 48,
+  },
+  info: {
+    marginTop: 12,
+  },
+});
+
+export default styles;
