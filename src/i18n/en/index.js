9e71adba0511858f18c45a3b24e3277dadc423cd
Ramiro Blázquez <ramirobg94@gmail.com>
2022-03-28 16:15:33 +0100

    add texts home

--- src/i18n/en/index.js
+++ src/i18n/en/index.js
@@ -2,12 +2,16 @@ import Common from './Common';
 import ConnectionSettings from './ConnectionSettings';
 import Home from './Home';
 import QRScanner from './QRScanner';
+import Survey from './Survey';
+import Surveys from './Surveys';
 
 const lang = {
   Common,
   ConnectionSettings,
   Home,
   QRScanner,
+  Survey,
+  Surveys,
 };
 
 export default lang;
