f2fe7121ada6d8f971cef17650fd24c50e1060cf
Ramiro Blázquez <ramirobg94@gmail.com>
2022-04-04 08:10:32 +0100

    change loged in

--- src/screens/Home/components/User/components/NotLoggedIn/component.js
+++ src/screens/Home/components/User/components/NotLoggedIn/component.js
@@ -2,20 +2,18 @@ import * as React from 'react';
 import {useTranslation} from 'react-i18next';
 
 import EmptyBlock from 'arena-mobile-ui/components/EmptyBlock';
-import {ROUTES} from 'navigation/constants';
 import {useNavigateTo} from 'navigation/hooks';
 
 const NotLoggedIn = () => {
   const {t} = useTranslation();
-  const navigateToConnectionSettings =
-    useNavigateTo[ROUTES.CONNECTION_SETTINGS]();
+  const {navigateTo, routes} = useNavigateTo();
 
   return (
     <EmptyBlock
       title={t('Home:not_logged_in.title')}
       info={t('Home:not_logged_in.info')}
-      label={t('Home:not_logged_in.cta')}
-      onPress={navigateToConnectionSettings}
+      ctaLabel={t('Home:not_logged_in.cta')}
+      onPress={navigateTo({route: routes.CONNECTION_SETTINGS})}
     />
   );
 };
