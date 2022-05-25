61eac184a888357bc22e6edf718d96da6bf04fd9
Ramiro Blázquez <ramirobg94@gmail.com>
2022-05-25 12:56:34 +0200

    add buttons

--- src/screens/Home/screen.js
+++ src/screens/Home/screen.js
@@ -1,16 +1,20 @@
 import * as React from 'react';
 import {Text, ScrollView} from 'react-native';
+import {useSelector} from 'react-redux';
 
 import Header from 'arena-mobile-ui/components/Header';
 import Layout from 'arena-mobile-ui/components/Layout';
 import baseStyles from 'arena-mobile-ui/styles';
 import NavigateToSettings from 'navigation/components/NavigateToSettings';
+import {selectors as surveySelectors} from 'state/survey';
 
+import Actions from './components/Actions';
 import Survey from './components/Survey';
 import User from './components/User';
 import styles from './styles';
 
 const Home = () => {
+  const survey = useSelector(surveySelectors.getSurvey);
   return (
     <Layout>
       <>
@@ -21,6 +25,7 @@ const Home = () => {
           <User />
           <Survey />
         </ScrollView>
+        {survey && <Actions />}
       </>
     </Layout>
   );
