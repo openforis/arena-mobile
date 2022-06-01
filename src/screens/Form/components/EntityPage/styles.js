929368bab362190143c8eac378e2d6babe73c2ee
Ramiro Blázquez <ramirobg94@gmail.com>
2022-06-01 13:12:54 +0200

    add entity page

---
+++ src/screens/Form/components/EntityPage/styles.js
@@ -0,0 +1,14 @@
+import {StyleSheet} from 'react-native';
+
+import * as colors from 'arena-mobile-ui/colors';
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    color: colors.neutralDarkest,
+    padding: 12,
+    flex: 1,
+  },
+});
+
+export default styles;
