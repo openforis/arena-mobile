a3e3da5763042317ec2a0caeb2ffe8b6acb099b4
Ramiro Blázquez <ramirobg94@gmail.com>
2022-06-02 13:11:24 +0200

    add attributes form container

---
+++ src/screens/Form/components/EntityPage/components/Entity/components/Attributes/component.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {Text, ScrollView, View} from 'react-native';
+import {useSelector} from 'react-redux';
+
+import AttributeLabel from 'form/common/Label';
+import {selectors as formSelectors} from 'state/form';
+import {selectors as surveySelectors} from 'state/survey';
+
+const Attribute = ({nodeDefUuid}) => <Text>{nodeDefUuid}</Text>;
+
+import styles from './styles';
+const Attributes = () => {
+  const nodeDef = useSelector(formSelectors.getParentEntityNodeDef);
+  const node = useSelector(formSelectors.getParentEntityNode);
+  const nodeDefChildrenUuids = useSelector(state =>
+    surveySelectors.getNodeDefEntityChildrenAttributesUuids(state, nodeDef),
+  );
+
+  return (
+    <ScrollView style={styles.container}>
+      <AttributeLabel nodeDef={nodeDef} />
+      <Text>{node.uuid}</Text>
+      {nodeDefChildrenUuids?.map(childrenUuid => (
+        <Attribute key={childrenUuid} nodeDefUuid={childrenUuid} />
+      ))}
+      <View style={{height: 100}} />
+    </ScrollView>
+  );
+};
+
+export default Attributes;
