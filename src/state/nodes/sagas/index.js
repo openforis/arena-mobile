b3d20ca001b255639e366f55093b942f96f81dfa
Ramiro Blázquez <ramirobg94@gmail.com>
2022-06-03 11:17:19 +0200

    fix node sagas

--- src/state/nodes/sagas/index.js
+++ src/state/nodes/sagas/index.js
@@ -2,8 +2,10 @@ import {takeLatest} from 'redux-saga/effects';
 
 import nodeActionTypes from '../actionTypes';
 
+import handleCreateNode from './createNode';
 import handleUpdateNode from './updateNode';
 
 export default function* () {
   yield takeLatest(nodeActionTypes.updateNode$, handleUpdateNode);
+  yield takeLatest(nodeActionTypes.createNode$, handleCreateNode);
 }
