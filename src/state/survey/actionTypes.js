c6c3d8ddb72edd5c9c8e22d57dbf39085a9f2da7
Ramiro Blázquez <ramirobg94@gmail.com>
2022-06-10 12:11:46 +0200

    prepare data

--- src/state/survey/actionTypes.js
+++ src/state/survey/actionTypes.js
@@ -2,7 +2,7 @@ const SET_SURVEY = 'survey/SET_SURVEY';
 const CLEAN_SURVEY = 'survey/CLEAN_SURVEY';
 
 const selectSurvey$ = 'survey/selectSurvey';
-
+const uploadSurveyData$ = 'survey/uploadSurveyData';
 const deleteSurveyData$ = 'survey/deleteSurveyData';
 
 const SELECT_SURVEY_LANGUAGE = 'survey/SELECT_SURVEY_LANGUAGE';
@@ -10,6 +10,7 @@ const SELECT_SURVEY_LANGUAGE = 'survey/SELECT_SURVEY_LANGUAGE';
 export default {
   selectSurvey$,
 
+  uploadSurveyData$,
   deleteSurveyData$,
 
   SET_SURVEY,
