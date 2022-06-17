860f7c8af9b686ae44e3c6671f13104c3fb74e96
Ramiro Blázquez <ramirobg94@gmail.com>
2022-06-17 13:17:46 +0200

    and base attribute

---
+++ src/form/Attributes/common/BaseInput/Preview/styles.js
@@ -0,0 +1,18 @@
+import {StyleSheet} from 'react-native';
+
+import * as colors from 'arena-mobile-ui/colors';
+
+const styles = StyleSheet.create({
+  container: {
+    marginBottom: 16,
+  },
+  nodeContainer: {
+    borderColor: colors.neutralLighter,
+    borderWidth: 1,
+    justifyContent: 'center',
+    padding: 8,
+    alignItems: 'flex-end',
+  },
+});
+
+export default styles;
