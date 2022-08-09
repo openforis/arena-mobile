6ab52640b16019c37bc8c18e9df93352c685eca5
Ramiro Blázquez <ramirobg94@gmail.com>
2022-08-09 20:06:10 +0200

    import records

--- src/state/records/api/api.js
+++ src/state/records/api/api.js
@@ -6,9 +6,6 @@ const getRecords = async ({serverUrl, surveyId}) => {
     const res = await API({serverUrl}).get({
       path: `api/survey/${surveyId}/records`,
     });
-    if (res.status === 204) {
-      return true;
-    }
     return res?.data?.list;
   } catch (error) {
     console.log('Error::recordApi:getRecords', {error});
