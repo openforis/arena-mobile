765f395b627bd45aa6f7c3e69009b26d60b1c50a
Ramiro Blázquez <ramirobg94@gmail.com>
2022-08-17 12:24:11 +0200

    fixes

--- src/infra/zip/index.js
+++ src/infra/zip/index.js
@@ -2,5 +2,5 @@ import * as ZIP from 'react-native-zip-archive';
 
 import {BASE_PATH} from 'infra/fs';
 
-export const zip = async ({source, destination}) =>
-  ZIP.zip(`${BASE_PATH}/${source}`, `${BASE_PATH}/${destination}`);
+export const zip = async ({source, destination, base = BASE_PATH}) =>
+  ZIP.zip(`${base}${source ? '/' : ''}${source}`, `${base}/${destination}`);
