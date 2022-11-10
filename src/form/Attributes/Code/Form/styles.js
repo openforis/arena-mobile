8392bb145eeba327fe3a7192124550406d7ead01
Ramiro Blázquez <ramirobg94@gmail.com>
2022-11-10 14:35:12 +0200

    search

--- src/form/Attributes/Code/Form/styles.js
+++ src/form/Attributes/Code/Form/styles.js
@@ -1,9 +1,33 @@
 import {StyleSheet} from 'react-native';
 
+import * as colors from 'arena-mobile-ui/colors';
 import baseStyles from 'arena-mobile-ui/styles';
 
 const styles = StyleSheet.create({
+  container: {flex: 1},
   containerStyle: {justifyContent: 'space-around'},
+  selectBarContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  selectContainer: {
+    flex: 1,
+  },
+  select: {
+    marginBottom: baseStyles.bases.BASE,
+  },
+  selectText: {
+    marginBottom: baseStyles.bases.BASE,
+  },
+  searchBox: {
+    marginLeft: 0,
+    marginBottom: baseStyles.bases.BASE,
+    flex: 1,
+  },
+  close: {
+    padding: baseStyles.bases.BASE_4,
+  },
   text: {
     textAlign: 'left',
     flex: 1,
@@ -12,6 +36,15 @@ const styles = StyleSheet.create({
   },
   selected: {
     fontWeight: '400',
+  },
+  card: {
+    borderColor: colors.neutralLight,
+    borderWidth: 1,
+    padding: baseStyles.bases.BASE_4,
+  },
+  selectedItem: {
+    backgroundColor: colors.secondary,
+    color: 'white',
   },
 });
 export default styles;
