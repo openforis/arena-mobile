557b4998350de2457fe991aabe26bb19066c10c5
Ramiro Blázquez <ramirobg94@gmail.com>
2022-11-29 17:51:09 +0200

    add listeners

--- src/screens/Form/screen.js
+++ src/screens/Form/screen.js
@@ -1,5 +1,6 @@
-import * as React from 'react';
-import {View} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import React, {useEffect} from 'react';
+import {Alert, View, BackHandler} from 'react-native';
 
 import Layout from 'arena-mobile-ui/components/Layout';
 import AttributeForm from 'form/common/Form';
@@ -9,7 +10,66 @@ import EntityPage from './components/EntityPage';
 import EntitySelector from './components/EntitySelector';
 import styles from './styles';
 
+/*
+// root:
+ - back -> ask
+ - leave -> ask
+// child
+- back -> Navigate to prev
+- leave -> ask
+*/
+const useAskBeforeLeave = () => {
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    const beforeRemoveAction = e => {
+      e.preventDefault();
+
+      // Prompt the user before leaving the screen
+      Alert.alert(
+        'Discard changes?',
+        'You have unsaved changes. Are you sure to discard them and leave the screen?',
+        [
+          {text: "Don't leave", style: 'cancel', onPress: () => {}},
+          {
+            text: 'Discard',
+            style: 'destructive',
+
+            onPress: () => navigation.dispatch(e.data.action),
+          },
+        ],
+      );
+    };
+
+    navigation.addListener('beforeRemove', beforeRemoveAction);
+
+    return () => navigation.removeListener('beforeRemove', beforeRemoveAction);
+  }, [navigation]);
+
+  useEffect(() => {
+    const backAction = () => {
+      Alert.alert('Hold on!', 'Are you sure you want to go back?', [
+        {
+          text: 'Cancel',
+          onPress: () => null,
+          style: 'cancel',
+        },
+        {text: 'YES', onPress: () => navigation.goBack()},
+      ]);
+      return true;
+    };
+
+    const backHandler = BackHandler.addEventListener(
+      'hardwareBackPress',
+      backAction,
+    );
+
+    return () => backHandler?.remove();
+  }, [navigation]);
+};
+
 const Form = () => {
+  useAskBeforeLeave();
   return (
     <>
       <Layout bottomSafeArea={false}>
