c32ad195e3b4d7bad54d6954ac48204c30a02201
Ramiro Blázquez <ramirobg94@gmail.com>
2022-12-05 18:52:39 +0200

    max size of checkbox code is 15

--- src/form/Attributes/Code/Preview/index.js
+++ src/form/Attributes/Code/Preview/index.js
@@ -6,17 +6,25 @@ import surveySelectors from 'state/survey/selectors';
 
 import CodeCheckbox from './CodeCheckbox';
 import CodeDropdown from './CodeDropdown';
+import {useCode} from './hooks';
 
 // TODO move to arena-core, maybe other name
 NodeDefs.getLayoutRenderTypePerCycle = ({nodeDef, cycle = 0}) =>
   nodeDef?.props.layout[cycle].renderType;
 
+const MAX_NUMBER_ITEMS_FOR_CHECKBOX = 15;
+
 const Preview = ({nodeDef}) => {
   const cycle = useSelector(surveySelectors.getSurveyCycle);
+  const {categoryItems} = useCode({nodeDef});
+
   if (nodeDef.analysis) {
     return <></>;
   }
-  if (NodeDefs.getLayoutRenderTypePerCycle({nodeDef, cycle}) === 'checkbox') {
+  if (
+    NodeDefs.getLayoutRenderTypePerCycle({nodeDef, cycle}) === 'checkbox' &&
+    categoryItems.length < MAX_NUMBER_ITEMS_FOR_CHECKBOX
+  ) {
     return <CodeCheckbox nodeDef={nodeDef} />;
   }
 
