3285f8797e9595a44c119682d46b26ced2ab48c4
Ramiro Blázquez <ramirobg94@gmail.com>
2022-12-06 17:12:44 +0200

    allow insert 0

--- src/form/Attributes/common/BaseInput/Preview/styles.js
+++ src/form/Attributes/common/BaseInput/Preview/styles.js
@@ -1,5 +1,12 @@
 import {StyleSheet} from 'react-native';
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  textAlignLeft: {
+    textAlign: 'left',
+  },
+  textAlignRight: {
+    textAlign: 'right',
+  },
+});
 
 export default styles;
