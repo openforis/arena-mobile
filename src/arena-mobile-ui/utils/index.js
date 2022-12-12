e9938ae0bd11d5f13d7c29af8fdf94a91cef9794
Ramiro Blázquez <ramirobg94@gmail.com>
2022-12-12 18:29:11 +0200

    prepare prompt component

--- src/arena-mobile-ui/utils/index.js
+++ src/arena-mobile-ui/utils/index.js
@@ -1,14 +1,5 @@
 import {Alert, Platform} from 'react-native';
-
-const OPTIONS_BY_OS = {
-  android: {cancelable: false},
-  ios: {},
-};
-
-const ALERT_FUNCTION_BY_OS = {
-  ios: Alert.prompt,
-  android: Alert.alert,
-};
+import prompt from 'react-native-prompt-android';
 
 export const alert = ({
   title,
@@ -19,6 +10,8 @@ export const alert = ({
   onDismiss = () => null,
   dismissStyle = 'cancel',
   acceptStyle = 'default',
+  requiredText = false,
+  requiredTextMessage = '',
 }) => {
   const buttons = [
     dismissText && {
@@ -30,13 +23,42 @@ export const alert = ({
     },
     acceptText && {
       text: acceptText,
-      onPress: () => {
+      onPress: (inputText = null) => {
+        if (requiredText && inputText !== requiredText) {
+          return;
+        }
         onAccept?.();
       },
       style: acceptStyle,
     },
   ];
 
-  const _alert = ALERT_FUNCTION_BY_OS?.[Platform.OS];
-  _alert(title, message, buttons, OPTIONS_BY_OS?.[Platform.OS]);
+  const _message = `
+    ${message}
+    
+    ${requiredTextMessage}`;
+
+  if (Platform.OS === 'ios') {
+    const type = requiredText ? 'plain-text' : 'default';
+    const defaultValue = '';
+    const keyboardType = 'default';
+
+    return Alert.prompt(
+      title,
+      _message,
+      buttons,
+      type,
+      defaultValue,
+      keyboardType,
+    );
+  }
+  if (requiredText) {
+    prompt(title, _message, buttons, {
+      type: 'text',
+      cancelable: false,
+      defaultValue: '',
+    });
+  }
+
+  return Alert.alert(title, message, buttons, {cancelable: false});
 };
