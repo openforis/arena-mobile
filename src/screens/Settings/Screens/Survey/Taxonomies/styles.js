d036cb696325a733d85ca98000cd112a1076cbf8
Ramiro Blázquez <ramirobg94@gmail.com>
2023-04-18 18:31:04 +0100

    translations and styles

--- src/screens/Settings/Screens/Survey/Taxonomies/styles.js
+++ src/screens/Settings/Screens/Survey/Taxonomies/styles.js
@@ -1,5 +1,6 @@
 import {StyleSheet} from 'react-native';
 
+import * as colors from 'arena-mobile-ui/colors';
 import baseStyles from 'arena-mobile-ui/styles';
 
 const styles = StyleSheet.create({
@@ -22,6 +23,32 @@ const styles = StyleSheet.create({
   example: {
     ...baseStyles.textStyle.header,
     textAlign: 'center',
+  },
+  optionContainer: {
+    backgroundColor: colors.white,
+    padding: baseStyles.bases.BASE_4,
+    borderBottomColor: colors.neutralLighter,
+    borderBottomWidth: 0.5,
+    flexDirection: 'row',
+  },
+  optionContainerFirst: {
+    marginTop: baseStyles.bases.BASE_4,
+    borderTopLeftRadius: baseStyles.bases.BASE_2,
+    borderTopRightRadius: baseStyles.bases.BASE_2,
+  },
+  optionContainerLast: {
+    borderBottomLeftRadius: baseStyles.bases.BASE_2,
+    borderBottomRightRadius: baseStyles.bases.BASE_2,
+    borderBottomWidth: 0,
+  },
+  optionContainerSelected: {
+    backgroundColor: colors.secondaryLighter,
+  },
+  optionText: {
+    marginLeft: baseStyles.bases.BASE_2,
+  },
+  optionTextSelected: {
+    fontWeight: 'bold',
   },
 });
 
