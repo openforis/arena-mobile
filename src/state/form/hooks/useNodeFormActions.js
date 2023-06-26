530c93bfcbbb5f9a984df1bc6b754cc3d91c9c9a
Ramiro Blázquez <ramirobg94@gmail.com>
2023-06-26 16:15:36 +0200

    extract hook

--- src/state/form/hooks/useNodeFormActions.js
+++ src/state/form/hooks/useNodeFormActions.js
@@ -1,10 +1,14 @@
+import {NodeDefs} from '@openforis/arena-core';
 import {useCallback} from 'react';
 import {useTranslation} from 'react-i18next';
 import {useSelector, useDispatch} from 'react-redux';
 
+import useNodeDefNameOrLabel from 'arena-mobile-ui/hooks/useNodeDefNameOrLabel';
 import {alert} from 'arena-mobile-ui/utils';
+import {Objects} from 'infra/objectUtils';
 
 import nodesActions from '../../nodes/actionCreators';
+import surveySelectors from '../../survey/selectors';
 import formActions from '../actionCreators';
 import formSelectors from '../selectors';
 
@@ -106,6 +110,72 @@ const useNodeFormActions = ({nodeDef}) => {
   const handleDelete = useDeleteNode();
 
   return {handleCreate, handleClose, handleUpdate, handleDelete};
+};
+
+export const useSelectNodeAndNodeDef = ({
+  node,
+  nodeDef,
+  parentNodeDef = false,
+  parentNode = false,
+}) => {
+  const dispatch = useDispatch();
+  const {t} = useTranslation();
+
+  const parentEntityNodeDef = useSelector(formSelectors.getParentEntityNodeDef);
+
+  const _parentEntityNodeDef = parentNodeDef || parentEntityNodeDef;
+
+  const parentEntityNode = useSelector(formSelectors.getParentEntityNode);
+  const _parentEntityNode = parentNode || parentEntityNode;
+
+  const keys = useSelector(state =>
+    surveySelectors.getEntityNodeKeysAsString(state, _parentEntityNode),
+  );
+
+  const nodeDefName = useNodeDefNameOrLabel({
+    nodeDef: _parentEntityNodeDef,
+  });
+
+  const handleCreateNewNodeEntity = useCallback(() => {
+    dispatch(
+      formActions.createEntity({
+        nodeDef: _parentEntityNodeDef,
+        node: _parentEntityNode,
+      }),
+    );
+  }, [dispatch, _parentEntityNodeDef, _parentEntityNode]);
+
+  const handleSelectNodeAndNodeDef = useCallback(() => {
+    if (
+      NodeDefs.isMultiple(_parentEntityNodeDef) &&
+      nodeDef.props.key &&
+      !Objects.isEmpty(node.value)
+    ) {
+      alert({
+        title: t('Form:avoidOverWrite.alert.title'),
+        message: t('Form:avoidOverWrite.alert.message', {
+          name: `${nodeDefName} [${keys}]`,
+        }),
+        acceptText: t('Form:avoidOverWrite.alert.accept'),
+        dismissText: t('Form:avoidOverWrite.alert.dismiss'),
+        onAccept: handleCreateNewNodeEntity,
+        onDismiss: () => null,
+      });
+    } else {
+      dispatch(formActions.setNode({node: node}));
+    }
+  }, [
+    dispatch,
+    node,
+    _parentEntityNodeDef,
+    nodeDef,
+    t,
+    nodeDefName,
+    keys,
+    handleCreateNewNodeEntity,
+  ]);
+
+  return handleSelectNodeAndNodeDef;
 };
 
 export default useNodeFormActions;
