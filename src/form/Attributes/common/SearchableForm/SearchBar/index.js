7fba526392ca40f68af320571d5bc7dce44b9605
Ramiro Blázquez <ramirobg94@gmail.com>
2023-07-20 17:18:34 +0200

    surveys search bar

--- src/form/Attributes/common/SearchableForm/SearchBar/index.js
+++ src/form/Attributes/common/SearchableForm/SearchBar/index.js
@@ -1,34 +1,53 @@
-import React from 'react';
-import {View} from 'react-native';
+import throttle from 'lodash.throttle';
+import React, {useCallback, useState} from 'react';
+import {Keyboard, View} from 'react-native';
 
 import Input from 'arena-mobile-ui/components/Input';
 import {TouchableIcon} from 'arena-mobile-ui/components/TouchableIcons';
 
 import styles from './styles';
 
-const SearchBar = ({handleStopToSearch, setSearchText}) => {
+const SearchBar = ({handleStopToSearch, setSearchText, autoFocus}) => {
+  const [clear, setClear] = useState(false);
+
+  const _handleStopToSearch = useCallback(() => {
+    setClear(Math.random());
+    setSearchText('');
+    Keyboard.dismiss();
+    handleStopToSearch?.();
+  }, [handleStopToSearch, setSearchText]);
+
+  const deboundedSearch = useCallback(
+    text => throttle(setSearchText, 500)(text),
+    [setSearchText],
+  );
   return (
     <View style={styles.selectBarContainer}>
       <View style={styles.selectContainer}>
         <Input
-          onChangeText={setSearchText}
+          onChangeText={deboundedSearch}
           defaultValue={String('')}
-          autoFocus={true}
+          autoFocus={autoFocus}
           horizontal={true}
           stacked={false}
           customStyle={styles.searchBox}
           hasTitle={false}
           onBlur={handleStopToSearch}
           returnKeyType="done"
+          clear={clear}
         />
       </View>
       <TouchableIcon
         iconName={'close'}
-        onPress={handleStopToSearch}
+        onPress={_handleStopToSearch}
         customStyle={styles.close}
       />
     </View>
   );
+};
+
+SearchBar.defaultValue = {
+  autoFocus: true,
 };
 
 export default SearchBar;
