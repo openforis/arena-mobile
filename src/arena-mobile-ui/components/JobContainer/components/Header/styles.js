b623fbed5bb001fe5e20c2e25070c71e03cf4076
Ramiro Blázquez <ramirobg94@gmail.com>
2023-07-31 14:21:07 +0200

    update modal styles

--- src/arena-mobile-ui/components/JobContainer/components/Header/styles.js
+++ src/arena-mobile-ui/components/JobContainer/components/Header/styles.js
@@ -1,15 +1,14 @@
 import {StyleSheet} from 'react-native';
 
-import baseStyles from 'arena-mobile-ui/styles';
-
-const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'row',
-    padding: baseStyles.bases.BASE_2,
-    paddingTop: baseStyles.bases.BASE_4,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
+const styles = ({baseStyles}) =>
+  StyleSheet.create({
+    container: {
+      flexDirection: 'row',
+      padding: baseStyles.bases.BASE_2,
+      paddingTop: baseStyles.bases.BASE_4,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+  });
 
 export default styles;
--- src/arena-mobile-ui/components/ProgressBar/styles.js
+++ src/arena-mobile-ui/components/ProgressBar/styles.js
@@ -1,29 +1,26 @@
 import {StyleSheet, Dimensions} from 'react-native';
 
 const {width} = Dimensions.get('screen');
-import * as colors from 'arena-mobile-ui/colors';
-import baseStyles from 'arena-mobile-ui/styles';
 
-const styles = StyleSheet.create({
-  barContainer: {
-    position: 'relative',
-    zIndex: 1,
-  },
-  background: {
-    borderRadius: baseStyles.bases.BASE_2,
-
-    backgroundColor: colors.neutralLighter,
-    width: width - baseStyles.bases.BASE_6,
-    justifyContent: 'center',
-    paddingRight: baseStyles.bases.BASE_3,
-  },
-  backgroundProgress: {
-    position: 'absolute',
-
-    borderRadius: baseStyles.bases.BASE_2,
-
-    backgroundColor: colors.success,
-  },
-});
+const styles = ({colors, baseStyles}) =>
+  StyleSheet.create({
+    barContainer: {
+      position: 'relative',
+      zIndex: 1,
+    },
+    background: {
+      borderRadius: baseStyles.bases.BASE_2,
+      backgroundColor: colors.neutralLighter,
+      width: width - baseStyles.bases.BASE_6,
+      justifyContent: 'center',
+      paddingRight: baseStyles.bases.BASE_3,
+    },
+    backgroundProgress: {
+      position: 'absolute',
+      borderRadius: baseStyles.bases.BASE_2,
+      backgroundColor: colors.success,
+    },
+    colors,
+  });
 
 export default styles;
