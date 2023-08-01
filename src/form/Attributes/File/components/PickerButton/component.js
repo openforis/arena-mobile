8500cd5bedccb17a7d3943c21618afd344e7b7cf
Ramiro Blázquez <ramirobg94@gmail.com>
2023-08-01 12:38:51 +0200

    prepare styles

--- src/form/Attributes/File/components/PickerButton/component.js
+++ src/form/Attributes/File/components/PickerButton/component.js
@@ -3,11 +3,13 @@ import {useTranslation} from 'react-i18next';
 
 import Button from 'arena-mobile-ui/components/Button';
 import Icon from 'arena-mobile-ui/components/Icon';
+import useThemedStyles from 'arena-mobile-ui/hooks/useThemedStyles';
 
-import styles from './styles';
+import _styles from './styles';
 
 const PickerButton = ({picker, handleByPickerType}) => {
   const {t} = useTranslation();
+  const styles = useThemedStyles(_styles);
   return (
     <Button
       label={t(`Form:nodeDefFile.pickers.${picker.label}`)}
--- src/form/Attributes/File/components/Preview/component.js
+++ src/form/Attributes/File/components/Preview/component.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import {Platform, View, Image} from 'react-native';
+import React, {useMemo} from 'react';
+import {Platform, View, Image, StyleSheet} from 'react-native';
 
+import TextBase from 'arena-mobile-ui/components/Texts/TextBase';
+import useThemedStyles from 'arena-mobile-ui/hooks/useThemedStyles';
 import * as fs from 'infra/fs';
 
-import styles from './styles';
+import _styles, {MaxPreviewSize} from './styles';
 
 const getFileUri = file => {
   const relativePath = fs.cleanPathWithBase(file.uri);
@@ -15,6 +17,7 @@ const getFileUri = file => {
 };
 
 export const FileImage = ({file}) => {
+  const styles = useThemedStyles(_styles);
   return (
     <Image
       resizeMode="cover"
@@ -27,12 +30,42 @@ export const FileImage = ({file}) => {
   );
 };
 
+const ImageStats = ({file}) => {
+  const fileSizeMB = useMemo(() => {
+    return String((file.meta.fileSize / 1024 || 0).toFixed(2));
+  }, [file]);
+
+  return (
+    <TextBase type="secondaryLight">
+      {fileSizeMB} KB · ({file.meta.width} x {file.meta.height} px)
+    </TextBase>
+  );
+};
+
 const Preview = ({file}) => {
+  const styles = useThemedStyles(_styles);
+  const imgContainerStyle = useMemo(() => {
+    return StyleSheet.compose(
+      styles.imgContainer,
+      file?.meta?.width && file?.meta?.height
+        ? {
+            width:
+              (file.meta.width / Math.min(file.meta.width, file.meta.height)) *
+              MaxPreviewSize,
+            height:
+              (file.meta.height / Math.min(file.meta.width, file.meta.height)) *
+              MaxPreviewSize,
+          }
+        : {},
+    );
+  }, [styles, file]);
+
   return (
     <View style={styles.container}>
-      <View style={styles.imgContainer}>
+      <View style={imgContainerStyle}>
         {file?.uri && <FileImage file={file} />}
       </View>
+      {file?.meta && <ImageStats file={file} />}
     </View>
   );
 };
