67ae86e576b106686346002579d6a92baf4a4a30
Ramiro Blázquez <ramirobg94@gmail.com>
2023-09-05 15:36:43 +0200

    Update src/arena-mobile-ui/components/CheckAppUpdateBar/hooks/index.js

--- src/arena-mobile-ui/components/CheckAppUpdateBar/hooks/index.js
+++ src/arena-mobile-ui/components/CheckAppUpdateBar/hooks/index.js
@@ -7,7 +7,7 @@ export const useGetAppHasUpdateAvailable = ({forceUpdate = false}) => {
   const checkVersion = useCallback(async () => {
     try {
       const _updateAvailable = await VersionCheck.needUpdate({forceUpdate});
-      setUpdateAvailable(_updateAvailable || {});
+      setUpdateAvailable(_updateAvailable);
     } catch (error) {
       console.log('useGetAppHasUpdateAvailable:checkVersion:error', {error});
     }
