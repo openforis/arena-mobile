df266c9331c6505d8ed897b6f319b7888e5ad4b2
Ramiro Blázquez <ramirobg94@gmail.com>
2023-10-05 13:51:04 +0200

    button to clean coordinates

--- src/form/Attributes/Coordinate/Preview/index.js
+++ src/form/Attributes/Coordinate/Preview/index.js
@@ -1,4 +1,4 @@
-import React, {useMemo} from 'react';
+import React, {useCallback, useMemo} from 'react';
 import {useTranslation} from 'react-i18next';
 import {View} from 'react-native';
 import openMap from 'react-native-open-maps';
@@ -7,15 +7,19 @@ import {useSelector} from 'react-redux';
 import Button from 'arena-mobile-ui/components/Button';
 import Icon from 'arena-mobile-ui/components/Icon';
 import LabelsAndValues from 'arena-mobile-ui/components/LabelsAndValues';
+import useThemedStyles from 'arena-mobile-ui/hooks/useThemedStyles';
 import BasePreview from 'form/Attributes/common/Base/Preview';
 import {Objects} from 'infra/objectUtils';
+import useNodeFormActions from 'state/form/hooks/useNodeFormActions';
 import formSelectors from 'state/form/selectors';
 import surveySelectors from 'state/survey/selectors';
 
+import _styles from './styles';
 const coordinateText = coordinate =>
   !isNaN(coordinate) && Number(coordinate).toFixed(4);
 
-const OpenInMap = ({nodeDef}) => {
+const CoordinateButtons = ({nodeDef, handleCleanCoordinates}) => {
+  const styles = useThemedStyles(_styles);
   const nodes = useSelector(state =>
     formSelectors.getNodeDefNodesInHierarchy(state, nodeDef),
   );
@@ -35,12 +39,17 @@ const OpenInMap = ({nodeDef}) => {
   }
 
   return (
-    <View>
+    <View style={styles.buttonsContainer}>
       <Button
         onPress={handleOpenMap}
         type="ghostBlack"
         label="Open in map"
         icon={<Icon name="map" size="s" />}
+      />
+      <Button
+        onPress={handleCleanCoordinates}
+        type="ghostBlack"
+        icon={<Icon name="trash-can-outline" size="s" />}
       />
     </View>
   );
@@ -48,9 +57,15 @@ const OpenInMap = ({nodeDef}) => {
 
 export const NodeValueRender = ({node, nodeDef}) => {
   const {t} = useTranslation();
-  const surveySrs = useSelector(surveySelectors.getSurveySRS);
-  const selectedSrs =
-    node?.value?.srs && surveySrs.find(srs => srs.code === node.value.srs);
+  const surveySrsIndex = useSelector(surveySelectors.getSurveySRSIndex);
+  const selectedSrs = node?.value?.srs && surveySrsIndex[node.value.srs];
+
+  const {handleClean} = useNodeFormActions({
+    nodeDef,
+  });
+  const handleCleanCoordinates = useCallback(() => {
+    handleClean({node});
+  }, [handleClean, node]);
 
   const items = useMemo(() => {
     return [
@@ -101,7 +116,10 @@ export const NodeValueRender = ({node, nodeDef}) => {
         size="m"
         items={items}
       />
-      <OpenInMap nodeDef={nodeDef} />
+      <CoordinateButtons
+        nodeDef={nodeDef}
+        handleCleanCoordinates={handleCleanCoordinates}
+      />
     </>
   );
 };
--- src/form/Attributes/Coordinate/Preview/styles.js
+++ src/form/Attributes/Coordinate/Preview/styles.js
@@ -1,5 +1,12 @@
 import {StyleSheet} from 'react-native';
 
-const styles = StyleSheet.create({});
+const styles = () =>
+  StyleSheet.create({
+    buttonsContainer: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+    },
+  });
 
 export default styles;
