ddab7ccef921819bd3089738938dcfe40fb3e3fa
Ramiro Blázquez <ramirobg94@gmail.com>
2024-01-16 15:08:01 +0200

    icon space

--- src/arena-mobile-ui/components/Icon/styles.js
+++ src/arena-mobile-ui/components/Icon/styles.js
@@ -4,6 +4,9 @@ const styles = ({colors, baseStyles}) =>
   StyleSheet.create({
     sizes: baseStyles.iconSizes,
     colors,
+    icon: {
+      paddingHorizontal: baseStyles.bases.BASE_2,
+    },
   });
 
 export default styles;
