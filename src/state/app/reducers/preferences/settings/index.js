200f86c77fba8e9f5a295df965f454abf7bec87f
Ramiro Blázquez <ramirobg94@gmail.com>
2024-02-27 13:39:21 +0200

    state

--- src/state/app/reducers/preferences/settings/index.js
+++ src/state/app/reducers/preferences/settings/index.js
@@ -2,8 +2,10 @@ import {combineReducers} from 'redux';
 
 import images from './images';
 import survey from './survey';
+import geo from './geo';
 
 export default combineReducers({
   images,
   survey,
+  geo,
 });
