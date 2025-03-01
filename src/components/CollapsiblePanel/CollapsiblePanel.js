2912771d7ddc5202fa3119029c1658ffd77ddef8
Stefano Ricci <SteRiccio@users.noreply.github.com>
2025-03-02 00:19:36 +0100

    records list options: layout adjustments

--- src/components/CollapsiblePanel/CollapsiblePanel.js
+++ src/components/CollapsiblePanel/CollapsiblePanel.js
@@ -11,8 +11,14 @@ import { VView } from "../VView";
 import { useStyles } from "./styles";
 
 export const CollapsiblePanel = (props) => {
-  const { children, containerStyle, headerContent, headerKey, headerParams } =
-    props;
+  const {
+    children,
+    containerStyle = null,
+    contentStyle = null,
+    headerContent,
+    headerKey,
+    headerParams,
+  } = props;
 
   const styles = useStyles();
 
@@ -44,7 +50,7 @@ export const CollapsiblePanel = (props) => {
       <Collapsible
         collapsed={collapsed}
         renderChildrenCollapsed={false}
-        style={styles.collapsibleContainer}
+        style={[styles.collapsibleContainer, contentStyle]}
       >
         {children}
       </Collapsible>
@@ -55,6 +61,7 @@ export const CollapsiblePanel = (props) => {
 CollapsiblePanel.propTypes = {
   children: PropTypes.node,
   containerStyle: PropTypes.object,
+  contentStyle: PropTypes.object,
   headerContent: PropTypes.node,
   headerKey: PropTypes.string,
   headerParams: PropTypes.object,
