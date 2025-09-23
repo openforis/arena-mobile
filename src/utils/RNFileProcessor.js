c27bf76ce538f9f698b99cb6da5d92e05f7fb3ae
Stefano Ricci <SteRiccio@users.noreply.github.com>
2025-09-23 22:21:06 +0200

    code cleanup

--- src/utils/RNFileProcessor.js
+++ src/utils/RNFileProcessor.js
@@ -4,10 +4,7 @@ import { Files } from "./Files";
 export class RNFileProcessor extends FileProcessor {
   async calculateFileSize() {
     const { filePath: fileUri } = this;
-    const size1 = await Files.getSize(fileUri);
-    // const size2 = await Files.getSizeBase64(fileUri);
-    // console.log("===size", size1, size2);
-    return size1;
+    return Files.getSize(fileUri);
   }
 
   async extractCurrentFileChunk() {
