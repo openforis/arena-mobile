0df9a1c8ef1b591e224df386de66d608083ef22a
SteRiccio <1219739+SteRiccio@users.noreply.github.com>
2025-10-29 14:55:45 +0100

    code cleanup

---
+++ src/state/reducers.ts
@@ -0,0 +1,27 @@
+import { combineReducers } from "redux";
+
+import { ConfirmReducer } from "./confirm";
+import { DataEntryReducer } from "./dataEntry";
+import { DeviceInfoReducer } from "./deviceInfo";
+import { JobMonitorReducer } from "./jobMonitor";
+import { MessageReducer } from "./message";
+import { RemoteConnectionReducer } from "./remoteConnection";
+import { ScreenOptionsReducer } from "./screenOptions";
+import { SettingsReducer } from "./settings";
+import { SurveyReducer } from "./survey";
+import { SurveyOptionsReducer } from "./surveyOptions";
+import { ToastReducer } from "./toast";
+
+export const rootReducer = combineReducers({
+  confirm: ConfirmReducer,
+  dataEntry: DataEntryReducer,
+  deviceInfo: DeviceInfoReducer,
+  jobMonitor: JobMonitorReducer,
+  message: MessageReducer,
+  remoteConnection: RemoteConnectionReducer,
+  settings: SettingsReducer,
+  screenOptions: ScreenOptionsReducer,
+  survey: SurveyReducer,
+  surveyOptions: SurveyOptionsReducer,
+  toast: ToastReducer,
+});
