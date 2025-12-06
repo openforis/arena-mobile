a70cc6b7af8bbb1549f350380e1dad7ee1effbb3
Stefano Ricci <SteRiccio@users.noreply.github.com>
2025-12-06 23:36:23 +0100

    URLs: added JSDoc

--- src/utils/URLs.ts
+++ src/utils/URLs.ts
@@ -3,6 +3,11 @@ import { Linking } from "react-native";
 const isValidUrl = async (url: string): Promise<boolean> =>
   Linking.canOpenURL(url);
 
+/**
+ * Ensures a URL starts with http:// or https://, defaulting to https:// if no protocol is present
+ * @param url The URL to normalize
+ * @returns The normalized URL or null if the URL is invalid
+ */
 const normalizeUrl = async (
   url: string | null | undefined
 ): Promise<string | null> => {
