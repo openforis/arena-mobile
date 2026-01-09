14d90a9aee278b3b9ea3593f8c24e0c06e019479
Stefano Ricci <SteRiccio@users.noreply.github.com>
2026-01-09 23:17:51 +0100

    location compass view refactoring

---
+++ src/screens/RecordEditor/NodeComponentSwitch/nodeTypes/NodeCoordinateComponent/CompassView.tsx
@@ -0,0 +1,143 @@
+import { useMemo } from "react";
+import { Dimensions, Image, StyleSheet } from "react-native";
+import { useTheme } from "react-native-paper";
+
+import { View } from "components";
+import { DeviceInfoSelectors } from "state";
+
+const assetsPath = "../../../../../../assets";
+const compassBgBlack = require(`${assetsPath}/compass_bg_black.png`);
+const compassBgWhite = require(`${assetsPath}/compass_bg_white.png`);
+const arrowUpGreen = require(`${assetsPath}/arrow_up_green.png`);
+const arrowUpOrange = require(`${assetsPath}/arrow_up_orange.png`);
+const arrowUpRed = require(`${assetsPath}/arrow_up_red.png`);
+const circleGreen = require(`${assetsPath}/circle_green.png`);
+
+const arrowToTargetVisibleDistanceThreshold = 30;
+
+const getArrowImageByAngle = (angle: number) => {
+  if (angle <= 20 || 360 - angle <= 20) return arrowUpGreen;
+  if (angle <= 45 || 360 - angle <= 45) return arrowUpOrange;
+  return arrowUpRed;
+};
+
+const { height, width } = Dimensions.get("window");
+const minDimension = Math.min(height, width);
+
+type CompassViewProps = {
+  distance: number;
+  heading: number;
+  angleToTargetDifference: number;
+};
+
+export const CompassView = (props: CompassViewProps) => {
+  const { angleToTargetDifference, distance, heading } = props;
+
+  const theme = useTheme();
+  const landscapeOrientation = DeviceInfoSelectors.useOrientationIsLandscape();
+
+  const compassBg = theme.dark ? compassBgWhite : compassBgBlack;
+
+  const arrowToTargetVisible =
+    distance >= arrowToTargetVisibleDistanceThreshold;
+
+  const dynamicStylesAndSizes = useMemo(() => {
+    const compassImageSize = landscapeOrientation
+      ? minDimension - 110
+      : minDimension - 40;
+
+    const targetLocationMarkerHeight = compassImageSize / 16;
+
+    const targetLocationBoxWidth = compassImageSize * 0.7;
+
+    const targetLocationBoxWidthAdjusted =
+      targetLocationBoxWidth *
+      (arrowToTargetVisible
+        ? 1
+        : distance / arrowToTargetVisibleDistanceThreshold);
+
+    return {
+      sizes: {
+        compassImageSize,
+        arrowToTargetHeight: compassImageSize * 0.7,
+        targetLocationBoxWidth,
+        targetLocationMarkerHeight,
+        targetLocationBoxMargin:
+          (compassImageSize -
+            targetLocationBoxWidth +
+            (targetLocationBoxWidth - targetLocationBoxWidthAdjusted)) /
+          2,
+        targetLocationBoxWidthAdjusted,
+      },
+      dynamicStyles: StyleSheet.create({
+        compassWrapper: {
+          height: compassImageSize,
+          width: compassImageSize,
+        },
+        targetLocationMarker: {
+          alignSelf: "center",
+          height: targetLocationMarkerHeight,
+          resizeMode: "contain",
+        },
+      }),
+    };
+  }, [arrowToTargetVisible, distance, landscapeOrientation]);
+
+  const { dynamicStyles, sizes } = dynamicStylesAndSizes;
+  const {
+    compassImageSize,
+    arrowToTargetHeight,
+    targetLocationBoxWidthAdjusted,
+    targetLocationBoxMargin,
+  } = sizes;
+
+  const arrowToTargetSource = getArrowImageByAngle(angleToTargetDifference);
+
+  return (
+    <View style={dynamicStyles.compassWrapper}>
+      <Image
+        source={compassBg}
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          height: compassImageSize,
+          width: compassImageSize,
+          resizeMode: "contain",
+          transform: [{ rotate: `${360 - heading} deg` }],
+        }}
+      />
+      {arrowToTargetVisible && (
+        <Image
+          source={arrowToTargetSource}
+          style={{
+            position: "absolute",
+            top: (compassImageSize - arrowToTargetHeight) / 2,
+            height: arrowToTargetHeight,
+            transform: [{ rotate: angleToTargetDifference + "deg" }],
+            resizeMode: "contain",
+            alignSelf: "center",
+          }}
+        />
+      )}
+      {!arrowToTargetVisible && (
+        <View
+          style={{
+            backgroundColor: "transparent",
+            width: targetLocationBoxWidthAdjusted,
+            height: targetLocationBoxWidthAdjusted,
+            position: "absolute",
+            top: targetLocationBoxMargin,
+            left: targetLocationBoxMargin,
+            transform: [{ rotate: angleToTargetDifference + "deg" }],
+          }}
+        >
+          <Image
+            source={circleGreen}
+            style={dynamicStyles.targetLocationMarker}
+          />
+        </View>
+      )}
+    </View>
+  );
+};
--- src/screens/RecordEditor/NodeComponentSwitch/nodeTypes/NodeCoordinateComponent/LocationNavigator.tsx
+++ src/screens/RecordEditor/NodeComponentSwitch/nodeTypes/NodeCoordinateComponent/LocationNavigator.tsx
@@ -1,10 +1,7 @@
 import { useCallback, useEffect, useState } from "react";
-import { Dimensions, Image, StyleSheet } from "react-native";
-import { useTheme } from "react-native-paper";
 
 import { Objects, Points } from "@openforis/arena-core";
 
-import { useLocationWatch, useMagnetometerHeading } from "hooks";
 import {
   Button,
   FlexWrapView,
@@ -12,51 +9,18 @@ import {
   HView,
   Modal,
   Text,
-  View,
   VView,
 } from "components";
+import { useLocationWatch, useMagnetometerHeading } from "hooks";
 import { LocationPoint } from "model";
 import { SurveySelectors } from "state";
-import { SystemUtils, log } from "utils";
+import { log } from "utils";
 
+import { CompassView } from "./CompassView";
 import styles from "./locationNavigatorStyles";
 
-const compassBgBlack = require(`../../../../../../assets/compass_bg_black.png`);
-const compassBgWhite = require(`../../../../../../assets/compass_bg_white.png`);
-const arrowUpGreen = require("../../../../../../assets/arrow_up_green.png");
-const arrowUpOrange = require("../../../../../../assets/arrow_up_orange.png");
-const arrowUpRed = require("../../../../../../assets/arrow_up_red.png");
-const circleGreen = require("../../../../../../assets/circle_green.png");
-
-const arrowToTargetVisibleDistanceThreshold = 30;
 const Symbols = {
   degree: "\u00b0",
-};
-
-const { height, width } = Dimensions.get("window");
-const minDimension = Math.min(height, width);
-
-const compassImageSize = minDimension - 40;
-const arrowToTargetHeight = compassImageSize * 0.7;
-const targetLocationBoxWidth = compassImageSize * 0.7;
-const targetLocationMarkerHeight = minDimension / 16;
-
-const dynamicStyles = StyleSheet.create({
-  compassWrapper: {
-    height: compassImageSize,
-    width: compassImageSize,
-  },
-  targetLocationMarker: {
-    alignSelf: "center",
-    height: targetLocationMarkerHeight,
-    resizeMode: "contain",
-  },
-});
-
-const getArrowImageByAngle = (angle: any) => {
-  if (angle <= 20 || 360 - angle <= 20) return arrowUpGreen;
-  if (angle <= 45 || 360 - angle <= 45) return arrowUpOrange;
-  return arrowUpRed;
 };
 
 const radsToDegrees = (rads: any) => {
@@ -99,11 +63,7 @@ export const LocationNavigator = (props: LocationNavigatorProps) => {
 
   log.debug(`rendering LocationNavigator`);
 
-  const theme = useTheme();
-
   const [state, setState] = useState(initialState);
-
-  const compassBg = theme.dark ? compassBgWhite : compassBgBlack;
 
   const srsIndex = SurveySelectors.useCurrentSurveySrsIndex();
 
@@ -143,25 +103,8 @@ export const LocationNavigator = (props: LocationNavigatorProps) => {
   const { longitude: currentLocationX, latitude: currentLocationY } =
     currentLocation ?? {};
 
-  const arrowToTargetVisible =
-    distance >= arrowToTargetVisibleDistanceThreshold;
-
-  const targetLocationBoxWidthAdjusted =
-    targetLocationBoxWidth *
-    (arrowToTargetVisible
-      ? 1
-      : distance / arrowToTargetVisibleDistanceThreshold);
-
-  const targetLocationBoxMargin =
-    (compassImageSize -
-      targetLocationBoxWidth +
-      (targetLocationBoxWidth - targetLocationBoxWidthAdjusted)) /
-    2;
-
   let angleToTargetDifference = angleToTarget - heading;
   if (angleToTargetDifference < 0) angleToTargetDifference += 360;
-
-  const arrowToTargetSource = getArrowImageByAngle(angleToTargetDifference);
 
   useEffect(() => {
     startLocationWatch();
@@ -199,51 +142,11 @@ export const LocationNavigator = (props: LocationNavigatorProps) => {
           resizeMode: "contain",
         }}
       /> */}
-          <View style={dynamicStyles.compassWrapper}>
-            <Image
-              source={compassBg}
-              style={{
-                position: "absolute",
-                top: 0,
-                left: 0,
-                height: compassImageSize,
-                width: compassImageSize,
-                resizeMode: "contain",
-                transform: [{ rotate: `${360 - heading} deg` }],
-              }}
-            />
-            {arrowToTargetVisible && (
-              <Image
-                source={arrowToTargetSource}
-                style={{
-                  position: "absolute",
-                  top: (compassImageSize - arrowToTargetHeight) / 2,
-                  height: arrowToTargetHeight,
-                  transform: [{ rotate: angleToTargetDifference + "deg" }],
-                  resizeMode: "contain",
-                  alignSelf: "center",
-                }}
-              />
-            )}
-            {!arrowToTargetVisible && (
-              <View
-                style={{
-                  backgroundColor: "transparent",
-                  width: targetLocationBoxWidthAdjusted,
-                  height: targetLocationBoxWidthAdjusted,
-                  position: "absolute",
-                  top: targetLocationBoxMargin,
-                  left: targetLocationBoxMargin,
-                  transform: [{ rotate: angleToTargetDifference + "deg" }],
-                }}
-              >
-                <Image
-                  source={circleGreen}
-                  style={dynamicStyles.targetLocationMarker}
-                />
-              </View>
-            )}
-          </View>
+          <CompassView
+            distance={distance}
+            heading={heading}
+            angleToTargetDifference={angleToTargetDifference}
+          />
           <VView>
             <HView style={styles.fieldsRow}>
               <FormItem labelKey="dataEntry:coordinate.accuracy">
