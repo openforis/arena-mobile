719d76f6e9daf47d38fcefa7f52118a71c5f8188
SteRiccio <1219739+SteRiccio@users.noreply.github.com>
2026-02-13 14:12:23 +0100

    code cleanup

--- src/components/useButtonPress.ts
+++ src/components/useButtonPress.ts
@@ -4,26 +4,36 @@ export const useButtonOnPress = ({
   avoidMultiplePress,
   loading,
   multiplePressAvoidanceTimeout = 500,
-  onPressProp
-}: any) => {
+  onPressProp,
+}: {
+  avoidMultiplePress?: boolean;
+  loading?: boolean;
+  multiplePressAvoidanceTimeout?: number;
+  onPressProp?: (event: any) => Promise<void> | void;
+}): { actualLoading: boolean; onPress: (event: any) => Promise<void> } => {
   const [temporaryLoading, setTemporaryLoading] = useState(false);
 
   const actualLoading = loading || temporaryLoading;
 
   const onPress = useCallback(
-    (event: any) => {
-      if (!actualLoading) {
-        if (avoidMultiplePress) {
-          setTemporaryLoading(true);
-          setTimeout(
-            () => setTemporaryLoading(false),
-            multiplePressAvoidanceTimeout
-          );
-        }
-        onPressProp(event);
+    async (event: any) => {
+      if (actualLoading) return;
+
+      if (avoidMultiplePress) {
+        setTemporaryLoading(true);
+        setTimeout(
+          () => setTemporaryLoading(false),
+          multiplePressAvoidanceTimeout,
+        );
       }
+      await onPressProp?.(event);
     },
-    [actualLoading, avoidMultiplePress, onPressProp]
+    [
+      actualLoading,
+      avoidMultiplePress,
+      multiplePressAvoidanceTimeout,
+      onPressProp,
+    ],
   );
 
   return { actualLoading, onPress };
