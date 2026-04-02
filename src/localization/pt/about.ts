84106a89c0374e11622215917690fb478a0222e4
Stefano Ricci <SteRiccio@users.noreply.github.com>
2026-04-02 21:03:49 +0200

    updated Potuguese translation

--- src/localization/pt/about.ts
+++ src/localization/pt/about.ts
@@ -1,7 +1,7 @@
 export default {
   developedBy: "Desenvolvido por",
   supportEmail: "E-mail de suporte",
-  supportForum: "Forum de suporte",
+  supportForum: "Fórum de suporte",
   support: "Suporte",
   version: "Versão",
   viewChangelog: "Ver registro de alterações",
