188233298be25f0585e18a5f95b745e97be11c57
Stefano Ricci <SteRiccio@users.noreply.github.com>
2026-04-17 21:15:44 +0200

    code cleanup

--- src/screens/RecordEditor/NodeDefFormItem/NodeDefFormItem.tsx
+++ src/screens/RecordEditor/NodeDefFormItem/NodeDefFormItem.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useMemo, useState } from "react";
 
 import { NodeDefs, Objects } from "@openforis/arena-core";
 
@@ -10,23 +10,66 @@ import {
 } from "state";
 import { log } from "utils";
 
-import { IconButton, Fade, VView } from "components";
+import { Fade, VView } from "components";
 import { RecordEditViewMode } from "model";
 
 import { CurrentRecordNodeValuePreview } from "../CurrentRecordNodeValuePreview";
 import { NodeComponentSwitch } from "../NodeComponentSwitch/NodeComponentSwitch";
 import { PreviousCycleNodeValuePreview } from "../PreviousCycleNodeValuePreview";
+import { KeyAttributeLockButton } from "./KeyAttributeLockButton";
 import { NodeDefFormItemHeader } from "./NodeDefFormItemHeader";
 
+import { NodeComponentProps } from "../NodeComponentSwitch/nodeTypes/nodeComponentPropTypes";
 import { useStyles } from "./styles";
 
-type NodeDefFormItemProps = {
-  nodeDef: any;
-  parentNodeUuid?: string;
-  onFocus?: () => void;
+const useKeyAttributeLock = ({
+  canEditRecord,
+  contentKey,
+  keyAttributeFilled,
+  keyAttributeLockAvailable,
+}: {
+  canEditRecord: boolean;
+  contentKey: string;
+  keyAttributeFilled: boolean;
+  keyAttributeLockAvailable: boolean;
+}) => {
+  const [keyAttributeLockState, setKeyAttributeLockState] = useState({
+    contentKey,
+    locked: keyAttributeFilled,
+  });
+
+  const keyAttributeLocked =
+    keyAttributeLockState.contentKey === contentKey
+      ? keyAttributeLockState.locked
+      : keyAttributeFilled;
+
+  const canDisplayKeyLockButton =
+    keyAttributeLockAvailable && canEditRecord && keyAttributeFilled;
+
+  const onKeyLockButtonPress = useCallback(
+    () =>
+      setKeyAttributeLockState((statePrev) => {
+        const lockedPrev =
+          statePrev.contentKey === contentKey
+            ? statePrev.locked
+            : keyAttributeFilled;
+
+        return {
+          contentKey,
+          locked: !lockedPrev,
+        };
+      }),
+    [contentKey, keyAttributeFilled],
+  );
+
+  return {
+    canDisplayKeyLockButton,
+    keyAttributeLocked,
+    onKeyLockButtonPress,
+  };
 };
 
-export const NodeDefFormItem = (props: NodeDefFormItemProps) => {
+export const NodeDefFormItem = (props: NodeComponentProps) => {
   const { nodeDef, parentNodeUuid, onFocus } = props;
 
   log.debug(`Rendering form item ${NodeDefs.getName(nodeDef)}`);
@@ -60,76 +103,85 @@ export const NodeDefFormItem = (props: NodeDefFormItemProps) => {
     DataEntrySelectors.useIsLinkedToPreviousCycleRecord();
   const canEditRecord = DataEntrySelectors.useCanEditRecord();
   const alwaysVisible = Objects.isEmpty(NodeDefs.getApplicable(nodeDef));
-  const [keyAttributeLockState, setKeyAttributeLockState] = useState({
-    contentKey,
-    locked: keyAttributeFilled,
-  });
 
   const includedInPreviousCycleLink =
     !NodeDefs.isKey(nodeDef) &&
     NodeDefs.isIncludedInPreviousCycleLink(cycle)(nodeDef);
 
-  const keyAttributeLocked =
-    keyAttributeLockState.contentKey === contentKey
-      ? keyAttributeLockState.locked
-      : keyAttributeFilled;
+  const { canDisplayKeyLockButton, keyAttributeLocked, onKeyLockButtonPress } =
+    useKeyAttributeLock({
+      canEditRecord,
+      contentKey,
+      keyAttributeFilled,
+      keyAttributeLockAvailable,
+    });
 
-  const keyAttributeLockButton =
-    canEditRecord && keyAttributeFilled ? (
-      <IconButton
-        icon={keyAttributeLocked ? "lock-outline" : "lock-open-variant-outline"}
-        onPress={() =>
-          setKeyAttributeLockState((statePrev) => {
-            const lockedPrev =
-              statePrev.contentKey === contentKey
-                ? statePrev.locked
-                : keyAttributeFilled;
-            return {
-              contentKey,
-              locked: !lockedPrev,
-            };
-          })
-        }
-        size={20}
-        style={styles.headerIconButton}
-      />
-    ) : null;
+  const keyAttributeLockButton = useMemo(
+    () =>
+      canDisplayKeyLockButton ? (
+        <KeyAttributeLockButton
+          keyAttributeLocked={keyAttributeLocked}
+          onPress={onKeyLockButtonPress}
+        />
+      ) : null,
+    [canDisplayKeyLockButton, keyAttributeLocked, onKeyLockButtonPress],
+  );
 
-  const formItemComponent = (
-    <VView
-      style={[
-        styles.formItem,
-        viewMode === RecordEditViewMode.oneNode ? styles.formItemOneNode : {},
-      ]}
-    >
-      <NodeDefFormItemHeader
-        nodeDef={nodeDef}
-        parentNodeUuid={parentNodeUuid}
-        startAccessory={keyAttributeLockButton}
-      />
-      <VView
-        style={[
-          styles.internalContainer,
-          viewMode === RecordEditViewMode.oneNode ? { flex: 1 } : {},
-        ]}
-      >
-        {isLinkedToPreviousCycleRecord && includedInPreviousCycleLink && (
-          <PreviousCycleNodeValuePreview nodeDef={nodeDef} />
-        )}
-        {canEditRecord && !keyAttributeLocked ? (
-          <NodeComponentSwitch
-            nodeDef={nodeDef}
-            parentNodeUuid={parentNodeUuid}
-            onFocus={onFocus}
-          />
-        ) : (
-          <CurrentRecordNodeValuePreview
-            nodeDef={nodeDef}
-            parentNodeUuid={parentNodeUuid}
-          />
-        )}
+  const formItemComponentStyle = useMemo(
+    () => [
+      styles.formItem,
+      viewMode === RecordEditViewMode.oneNode ? styles.formItemOneNode : {},
+    ],
+    [viewMode, styles.formItem, styles.formItemOneNode],
+  );
+
+  const internalContainerStyle = useMemo(
+    () => [
+      styles.internalContainer,
+      viewMode === RecordEditViewMode.oneNode ? { flex: 1 } : {},
+    ],
+    [viewMode, styles.internalContainer],
+  );
+
+  const formItemComponent = useMemo(
+    () => (
+      <VView style={formItemComponentStyle}>
+        <NodeDefFormItemHeader
+          nodeDef={nodeDef}
+          parentNodeUuid={parentNodeUuid}
+          startAccessory={keyAttributeLockButton}
+        />
+        <VView style={internalContainerStyle}>
+          {isLinkedToPreviousCycleRecord && includedInPreviousCycleLink && (
+            <PreviousCycleNodeValuePreview nodeDef={nodeDef} />
+          )}
+          {canEditRecord && !keyAttributeLocked ? (
+            <NodeComponentSwitch
+              nodeDef={nodeDef}
+              parentNodeUuid={parentNodeUuid}
+              onFocus={onFocus}
+            />
+          ) : (
+            <CurrentRecordNodeValuePreview
+              nodeDef={nodeDef}
+              parentNodeUuid={parentNodeUuid}
+            />
+          )}
+        </VView>
       </VView>
-    </VView>
+    ),
+    [
+      canEditRecord,
+      formItemComponentStyle,
+      includedInPreviousCycleLink,
+      internalContainerStyle,
+      isLinkedToPreviousCycleRecord,
+      keyAttributeLockButton,
+      keyAttributeLocked,
+      nodeDef,
+      onFocus,
+      parentNodeUuid,
+    ],
   );
 
   if (alwaysVisible) {
