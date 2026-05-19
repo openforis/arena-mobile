dc44d512a435462be18131adf09975ef8baa7e31
SteRiccio <1219739+SteRiccio@users.noreply.github.com>
2026-05-19 15:09:12 +0200

    layout adjustments

--- src/screens/RecordEditor/NodeComponentSwitch/nodeTypes/NodeCoordinateComponent/LocationNavigator.tsx
+++ src/screens/RecordEditor/NodeComponentSwitch/nodeTypes/NodeCoordinateComponent/LocationNavigator.tsx
@@ -128,7 +128,7 @@ export const LocationNavigator = (props: LocationNavigatorProps) => {
             heading={heading}
             angleToTarget={angleToTarget}
           />
-          <VView>
+          <VView fullFlex>
             <FormItem labelKey="dataEntry:coordinate.heading">
               {formatNumber(heading, 1, Symbols.degree)}
             </FormItem>
