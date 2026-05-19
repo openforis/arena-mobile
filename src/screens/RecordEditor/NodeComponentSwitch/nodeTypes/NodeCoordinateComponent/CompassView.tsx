a130e71a9a12b4b45f12f942b3b15485a53edbaa
Stefano Ricci <SteRiccio@users.noreply.github.com>
2026-05-19 19:09:58 +0200

    code cleanup

--- src/screens/RecordEditor/NodeComponentSwitch/nodeTypes/NodeCoordinateComponent/CompassView.tsx
+++ src/screens/RecordEditor/NodeComponentSwitch/nodeTypes/NodeCoordinateComponent/CompassView.tsx
@@ -1,23 +1,23 @@
 import { useMemo } from "react";
-import { Image, StyleSheet } from "react-native";
-import { useTheme } from "react-native-paper";
+import { Image } from "react-native";
 
 import { Numbers } from "@openforis/arena-core";
 
 import { View } from "components";
 import { useMinScreenDimension } from "hooks";
 import { DeviceInfoSelectors } from "state";
-import { ColorUtils } from "utils";
+import {
+  arrowToTargetVisibleDistanceThreshold,
+  useCompassColors,
+  useCompassStylesAndSizes,
+} from "./useCompassStylesAndSizes";
 
 const assetsPath = "../../../../../../assets";
-const compassBgBlack = require(`${assetsPath}/compass_bg_black.png`);
-const compassBgWhite = require(`${assetsPath}/compass_bg_white.png`);
+
 const arrowUpGreen = require(`${assetsPath}/arrow_up_green.png`);
 const arrowUpOrange = require(`${assetsPath}/arrow_up_orange.png`);
 const arrowUpRed = require(`${assetsPath}/arrow_up_red.png`);
 const circleGreen = require(`${assetsPath}/circle_green.png`);
-
-const arrowToTargetVisibleDistanceThreshold = 30;
 
 const getArrowImageByAngle = (angle: number) => {
   if (angle <= 20 || 360 - angle <= 20) {
@@ -41,131 +41,21 @@ type CompassViewProps = {
 export const CompassView = (props: CompassViewProps) => {
   const { angleToTarget, distance, heading } = props;
 
-  const theme = useTheme();
+  const minDimension = useMinScreenDimension();
   const landscapeOrientation = DeviceInfoSelectors.useOrientationIsLandscape();
 
-  const compassBg = theme.dark ? compassBgWhite : compassBgBlack;
-  const bearingTriangleColor = ColorUtils.withOpacity(
-    theme.colors.primary,
-    0.6,
-  );
-  const centerCrossColor = ColorUtils.withOpacity(theme.colors.onSurface, 0.7);
+  const { compassBg, bearingTriangleColor, centerCrossColor } =
+    useCompassColors();
 
   const arrowToTargetVisible =
     distance >= arrowToTargetVisibleDistanceThreshold;
 
-  const arrowToTargetAngle = Numbers.absMod(360)(angleToTarget - heading);
+  const arrowToTargetAngle = useMemo(
+    () => Numbers.absMod(360)(angleToTarget - heading),
+    [angleToTarget, heading],
+  );
 
-  const minDimension = useMinScreenDimension();
-
-  const dynamicStylesAndSizes = useMemo(() => {
-    const compassImageSize = landscapeOrientation
-      ? minDimension - 110
-      : minDimension - 40;
-    const bearingTriangleSize = compassImageSize * 0.042;
-    const bearingTriangleInset = compassImageSize * 0.014;
-
-    const targetLocationMarkerHeight = compassImageSize / 16;
-
-    const targetLocationBoxWidth = compassImageSize * 0.7;
-
-    const targetLocationBoxWidthAdjusted =
-      targetLocationBoxWidth *
-      (arrowToTargetVisible
-        ? 1
-        : distance / arrowToTargetVisibleDistanceThreshold);
-
-    const targetLocationBoxMargin =
-      (compassImageSize -
-        targetLocationBoxWidth +
-        (targetLocationBoxWidth - targetLocationBoxWidthAdjusted)) /
-      2;
-    return {
-      sizes: {
-        compassImageSize,
-        arrowToTargetHeight: compassImageSize * 0.7,
-        bearingTriangleInset,
-        bearingTriangleSize,
-        targetLocationBoxWidth,
-        targetLocationMarkerHeight,
-        targetLocationBoxMargin,
-        targetLocationBoxWidthAdjusted,
-      },
-      dynamicStyles: StyleSheet.create({
-        compassWrapper: {
-          height: compassImageSize,
-          width: compassImageSize,
-        },
-        compassBackground: {
-          position: "absolute",
-          top: 0,
-          left: 0,
-          height: compassImageSize,
-          width: compassImageSize,
-          resizeMode: "contain",
-          transform: [{ rotate: `${360 - heading} deg` }],
-        },
-        centerCross: {
-          position: "absolute",
-          top: (compassImageSize - 10) / 2,
-          left: (compassImageSize - 10) / 2,
-          width: 10,
-          height: 10,
-          alignItems: "center",
-          justifyContent: "center",
-        },
-        centerCrossHorizontal: {
-          position: "absolute",
-          width: 10,
-          height: 1.5,
-          borderRadius: 1,
-          backgroundColor: centerCrossColor,
-        },
-        centerCrossVertical: {
-          position: "absolute",
-          width: 1.5,
-          height: 10,
-          borderRadius: 1,
-          backgroundColor: centerCrossColor,
-        },
-        bearingTriangle: {
-          position: "absolute",
-          alignSelf: "center",
-          width: 0,
-          height: 0,
-          backgroundColor: "transparent",
-          borderLeftWidth: bearingTriangleSize,
-          borderRightWidth: bearingTriangleSize,
-          borderLeftColor: "transparent",
-          borderRightColor: "transparent",
-        },
-        bearingTriangleTop: {
-          top: bearingTriangleInset,
-          borderBottomWidth: bearingTriangleSize * 1.2,
-          borderBottomColor: bearingTriangleColor,
-        },
-        bearingTriangleBottom: {
-          bottom: bearingTriangleInset,
-          borderTopWidth: bearingTriangleSize * 1.2,
-          borderTopColor: bearingTriangleColor,
-        },
-        targetLocationMarkerWrapper: {
-          backgroundColor: "transparent",
-          width: targetLocationBoxWidthAdjusted,
-          height: targetLocationBoxWidthAdjusted,
-          position: "absolute",
-          top: targetLocationBoxMargin,
-          left: targetLocationBoxMargin,
-          transform: [{ rotate: angleToTarget + "deg" }],
-        },
-        targetLocationMarker: {
-          alignSelf: "center",
-          height: targetLocationMarkerHeight,
-          resizeMode: "contain",
-        },
-      }),
-    };
-  }, [
+  const dynamicStylesAndSizes = useCompassStylesAndSizes({
     angleToTarget,
     arrowToTargetVisible,
     bearingTriangleColor,
@@ -174,7 +64,7 @@ export const CompassView = (props: CompassViewProps) => {
     heading,
     landscapeOrientation,
     minDimension,
-  ]);
+  });
 
   const { dynamicStyles, sizes } = dynamicStylesAndSizes;
   const { compassImageSize, arrowToTargetHeight } = sizes;
