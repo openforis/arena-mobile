57dfbda7ff2cbb941bc7e4abbf19e80f1b42dfe7
SteRiccio <1219739+SteRiccio@users.noreply.github.com>
2026-07-08 12:38:50 +0200

    updated version number and changelog

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,7 @@
 import { ExpoConfig } from "expo/config";
 
-const appVersion = "2.6.3";
-const buildNumber = 102;
+const appVersion = "2.6.4";
+const buildNumber = 103;
 
 const basePlugins: ExpoConfig["plugins"] = [
   ["expo-asset", { assets: ["assets"] }],
