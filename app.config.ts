3c2138287cbe39101a46635640f658f242e042b7
Stefano Ricci <SteRiccio@users.noreply.github.com>
2026-07-27 23:14:00 +0200

    update build number

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,7 @@
 import { ExpoConfig } from "expo/config";
 
 const appVersion = "2.7.0";
-const buildNumber = 112;
+const buildNumber = 113;
 
 const basePlugins: ExpoConfig["plugins"] = [
   ["expo-asset", { assets: ["assets"] }],
