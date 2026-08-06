fdd8b6bf0bbd7fe123c04dc60f11ab68ed2949c7
SteRiccio <1219739+SteRiccio@users.noreply.github.com>
2026-08-06 13:35:27 +0200

    udpated build number

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,7 @@
 import { ExpoConfig } from "expo/config";
 
 const appVersion = "2.7.1";
-const buildNumber = 114;
+const buildNumber = 115;
 
 const basePlugins: ExpoConfig["plugins"] = [
   ["expo-asset", { assets: ["assets"] }],
